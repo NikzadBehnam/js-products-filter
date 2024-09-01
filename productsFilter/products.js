3daf0797cbc611881a2beb5e9aba2a8f8afb400b
NikzadBehnam <nikzad.behnam1992@gmail.com>
2024-09-01 17:30:00 +0200

    Update: products elements added dynamicaly on the HTML page using DOM

---
+++ productsFilter/products.js
@@ -0,0 +1,131 @@
+const products = [
+  // Games
+  {
+    name: "Sony Playstation 5",
+    url: "https://source.unsplash.com/random/300x200?playstation",
+    category: "games",
+    price: 499.99,
+  },
+  {
+    name: "Nintendo Switch",
+    url: "https://source.unsplash.com/random/300x200?nintendo",
+    category: "games",
+    price: 299.99,
+  },
+  {
+    name: "Xbox Series X",
+    url: "https://source.unsplash.com/random/300x200?xbox",
+    category: "games",
+    price: 499.99,
+  },
+  {
+    name: "Sony Playstation 4",
+    url: "https://source.unsplash.com/random/300x200?playstation",
+    category: "games",
+    price: 399.99,
+  },
+  {
+    name: "Nintendo Switch Lite",
+    url: "https://source.unsplash.com/random/300x200?nintendo",
+    category: "games",
+    price: 199.99,
+  },
+
+  // Smartphones
+  {
+    name: "Samsung Galaxy",
+    url: "https://source.unsplash.com/random/300x200?samsung",
+    category: "smartphones",
+    price: 399.99,
+  },
+  {
+    name: "Google Pixel",
+    url: "https://source.unsplash.com/random/300x200?pixel",
+    category: "smartphones",
+    price: 499.99,
+  },
+  {
+    name: "iPhone 14",
+    url: "https://source.unsplash.com/random/300x200?iphone",
+    category: "smartphones",
+    price: 999.99,
+  },
+  {
+    name: "OnePlus 9",
+    url: "https://source.unsplash.com/random/300x200?oneplus",
+    category: "smartphones",
+    price: 729.99,
+  },
+  {
+    name: "Sony Xperia",
+    url: "https://source.unsplash.com/random/300x200?sony",
+    category: "smartphones",
+    price: 799.99,
+  },
+
+  // Cameras
+  {
+    name: "Cannon EOS Camera",
+    url: "https://source.unsplash.com/random/300x200?camera",
+    category: "cameras",
+    price: 749.99,
+  },
+  {
+    name: "Sony A7 Camera",
+    url: "https://source.unsplash.com/random/300x200?camera",
+    category: "cameras",
+    price: 1999.99,
+  },
+  {
+    name: "Sony ZV1F Camera",
+    url: "https://source.unsplash.com/random/300x200?camera",
+    category: "cameras",
+    price: 799.99,
+  },
+  {
+    name: "Nikon D3500",
+    url: "https://source.unsplash.com/random/300x200?nikon",
+    category: "cameras",
+    price: 499.99,
+  },
+  {
+    name: "Fujifilm X-T30",
+    url: "https://source.unsplash.com/random/300x200?fujifilm",
+    category: "cameras",
+    price: 899.99,
+  },
+
+  // Televisions
+  {
+    name: "LG TV",
+    url: "https://source.unsplash.com/random/300x200?tv",
+    category: "televisions",
+    price: 799.99,
+  },
+  {
+    name: "Samsung TV",
+    url: "https://source.unsplash.com/random/300x200?tv",
+    category: "televisions",
+    price: 1099.99,
+  },
+  {
+    name: "Toshiba TV",
+    url: "https://source.unsplash.com/random/300x200?tv",
+    category: "televisions",
+    price: 499.99,
+  },
+  {
+    name: "Sony Bravia TV",
+    url: "https://source.unsplash.com/random/300x200?tv",
+    category: "televisions",
+    price: 1299.99,
+  },
+  {
+    name: "Vizio Smart TV",
+    url: "https://source.unsplash.com/random/300x200?tv",
+    category: "televisions",
+    price: 599.99,
+  },
+];
+
+export default products;
--- productsFilter/script.js
+++ productsFilter/script.js
@@ -1,1 +1,47 @@
-re
+import products from "./products.js";
+
+/* DOM Elements */
+const productsWrappers = document.querySelector("#product-wrapper");
+const cartCount = document.querySelector("#cart-count");
+const fitlersContainer = document.querySelector("#filters-container");
+const seachInput = document.querySelector("#search");
+const checkBoxes = document.querySelectorAll(".check");
+
+let cartItemCount = 0;
+const productElements = [];
+
+/* Loop over the products */
+products.forEach((product) => {
+  const productElement = document.createElement("div");
+  productElement.className =
+    "bg-gray-700 rounded-lg mx-4 p-6 flex-1 min-w-[280px] transform transition-transform duration-700";
+
+  productElement.innerHTML = `
+   <div class="overflow-hidden rounded-lg">
+      <img
+        src="https://via.placeholder.com/150"
+        alt="${product.name}"
+        class="w-full h-40 object-cover transform transition-transform duration-500 hover:scale-110"
+      />
+   
+    </div>
+    <div class="p-4">
+       <small class="text-white mb-2">${product.category.toUpperCase()}</small>
+
+      <p class="text-xl font-semibold text-white mb-2">${product.name}</p>
+      <p class="text-gray-300 mb-4"><span>$${product.price.toLocaleString()}</span></p>
+      <div class="flex items-center justify-between mt-4">
+        <div class="space-x-2">
+          <button
+            class="bg-gray-500 text-white px-4 py-2 rounded transition-colors duration-200"
+          >
+            Add to Cart
+          </button>
+        </div>
+      </div>
+    </div>
+  `;
+
+  productElements.push(productElement);
+  productsWrappers.appendChild(productElement);
+});
